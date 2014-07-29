02410b1b6e53fc459fd14ad74cfc7e95034a26f8
Joe McCall <joemccall@intrepid-networks.com>
2014-07-29 14:35:30 -0400

    Read preferences file

--- app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -21,10 +21,11 @@ public class SettingsActivity extends PreferenceActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Add 'general' preferences, defined in the XML file
-        // TODO: Add preferences from XML
+        addPreferencesFromResource(R.xml.pref_general);
+
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
-        // TODO: Add preferences
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
     }
 
     /**
